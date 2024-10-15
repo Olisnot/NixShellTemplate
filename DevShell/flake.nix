741b6bd406c831223836adbe32e05aae3d0617c5
Olisnot <oliverpm2007@gmail.com>
2024-10-15 13:31:49 +0100

    Learned how to make a template

---
+++ DevShell/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Development Shell";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixvimConfig.url = "github:Olisnot/NixVimConfig";
+  };
+
+  outputs = { self, nixpkgs, nixvimConfig }: 
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages."${system}";
+      nvim = nixvimConfig.packages."${system}".default.extend {
+      };
+    in
+    {
+      devShells."${system}".default = pkgs.mkShell {
+        packages = []; 
+
+        buildInputs = [ nvim ];
+
+        shellHook = ''
+        '';
+      };
+    };
+}
--- flake.nix
+++ flake.nix
@@ -2,26 +2,14 @@
   description = "Nix Shell template";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    nixvim.url = "github:Olisnot/NixVimConfig";
   };
 
-  outputs = { self, nixpkgs, nixvim }:
-  let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages."${system}";
-    nvim = nixvim.packages."${system}".default.extend {
-    };
-  in
+  outputs = { self }:
   {
     templates = {
-      devShells."${system}".default = pkgs.mkShell {
-        packages = with pkgs; [ ];
-
-        buildInputs = [ nvim ];
-
-        shellHook = ''
-        '';
+      DevShell= { 
+        path = ./DevShell;
+        description = "A plain Development Shell";
       };
     };
   };
