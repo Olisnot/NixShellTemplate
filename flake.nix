0815b28f6393f90dd9f5b1690dda202191bd298e
Olisnot <oliverpm2007@gmail.com>
2024-10-15 13:09:21 +0100

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Nix Shell template";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixvim.url = "github:Olisnot/NixVimConfig";
+  };
+
+  outputs = { self, nixpkgs, nixvim }:
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages."${system}";
+    nvim = nixvim.packages."${system}".default.extend {
+    };
+  in
+  {
+    devShells."${system}".default = pkgs.mkShell {
+      packages = with pkgs; [ ];
+
+      buildInputs = [ nvim ];
+
+      shellHook = ''
+      '';
+    };
+  };
+
+}
