35ad2793b91c1f223a175e3c10c73da258550a6a
Olisnot <oliverpm2007@gmail.com>
2025-03-17 20:19:31 +0000

    changing rust shell to use rustaceanvim

--- rust/flake.nix
+++ rust/flake.nix
@@ -7,26 +7,27 @@
   };
 
   outputs = { self, nixpkgs, nixvimConfig }: 
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages."${system}";
-      nvim = nixvimConfig.packages."${system}".default.extend {
-        plugins.lsp.servers.rust_analyzer = {
-          enable = true;
-          installRustc = false;
-          installCargo = false;
-        };
-      };
-    in
-    {
-      devShells."${system}".default = pkgs.mkShell {
-        buildInputs = /* bash */ [ nvim ];
-        nativeBuildInputs = with pkgs; [ rustc cargo gcc rustfmt clippy ];
-        RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
-
-        shellHook = ''
-        export SHELL=/run/current-system/sw/bin/bash
-        '';
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages."${system}";
+    nvim = nixvimConfig.packages."${system}".default.extend {
+      plugins = {
+        rustaceanvim.enable = true;
+        dap.enable = true;
+        dap-ui.enable = true;
+        dap-lldb.enable = true;
       };
     };
+  in
+  {
+    devShells."${system}".default = pkgs.mkShell {
+      buildInputs = /* bash */ [ nvim ];
+      nativeBuildInputs = with pkgs; [ rustc cargo gcc rustfmt clippy ];
+      RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+
+      shellHook = ''
+      export SHELL=/run/current-system/sw/bin/bash
+      '';
+    };
+  };
 }
